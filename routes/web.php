fe12df9d7de9665817bd0ee89349bb5712c0bf62
AiDinaAgustin <aidinagustin2@gmail.com>
2023-11-23 16:58:49 +0700

    perubahan awal

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/resume', function () {
+    return view('resume');
+});
+
+Route::get('/projects', function () {
+    return view('projects');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
