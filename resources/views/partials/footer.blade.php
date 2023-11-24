7207589e9d64865ec67ed80ab204081c87cdc6fd
AiDinaAgustin <aidinagustin2@gmail.com>
2023-11-24 22:14:19 +0700

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,14 +1,64 @@
-<footer class="bg-white py-4 mt-auto">
+
+<footer class="bg-gradient-primary-to-secondary py-4 mt-auto">
     <div class="container px-5">
-        <div class="row align-items-center justify-content-between flex-column flex-sm-row">
-            <div class="col-auto"><div class="small m-0">Copyright &copy; Ai Dina Agustin 2023</div></div>
-            <div class="col-auto">
-                <a class="small" href="#!">Privacy</a>
-                <span class="mx-1">&middot;</span>
-                <a class="small" href="#!">Terms</a>
-                <span class="mx-1">&middot;</span>
-                <a class="small" href="#!">Contact</a>
+          <div class="row">
+            <div class="col-6 col-md-2 mb-3">
+              <h5 class="text-white mb-3">Portofolio</h5>
+              <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">About</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Resume</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Project</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">About</a></li>
+              </ul>
             </div>
-        </div>
-    </div>
-</footer>
+      
+            <div class="col-6 col-md-2 mb-3">
+            <h5 class="text-white mb-3">Skills</h5>
+              <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white"></a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Features</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Pricing</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">FAQs</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">About</a></li>
+              </ul>
+            </div>
+      
+            <div class="col-6 col-md-2 mb-3">
+            <h5 class="text-white mb-3">Experience</h5>
+              <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Home</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Features</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Pricing</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">FAQs</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">About</a></li>
+              </ul>
+            </div>
+      
+            <div class="col-md-5 offset-md-1 mb-3">
+              <form>
+                <h5 class="text-white mb-3">Our Address</h5>
+                <p class="text-white">aidinaagustin2@gmail.com <br>Jl. Raya Cibolang No.21 Cisaat Sukabumi 43152
+                    Indonesia</p>
+              </form>
+              <div class="">
+                <a class="text-black px-0" href="#!"><i class="bi bi-github fs-5"></i></a>
+                <a class="text-black px-2" href="#!"><i class="bi bi-instagram fs-5"></i></a>
+                <a class="text-black px-2" href="#!"><i class="bi bi-facebook fs-5"></i></a>
+                <a class="text-black px-2" href="#!"><i class="bi bi-linkedin fs-5"></i></a>
+              </div>
+            </div>
+          </div>
+      
+          <div class="d-flex flex-column flex-sm-row justify-content-between py-2 border-top">
+            <p class="text-white">Ai Dina Agustin &copy; 2023 Inc. All rights reserved.</p>
+            <div class="col-auto text-white">
+                <a class="small text-white text-decoration-none" href="#!">Privacy</a>
+                <span class="mx-1">&middot;</span>
+                <a class="small text-white text-decoration-none" href="#!">Terms</a>
+                <span class="mx-1">&middot;</span>
+                <a class="small text-white text-decoration-none" href="#!">Contact</a>
+            </div>
+          </div>
+
+      </div>
+    </footer>
