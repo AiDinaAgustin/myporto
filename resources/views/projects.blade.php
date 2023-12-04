066b66c68a24774a15bba8a261bc24e16ee1d0be
AiDinaAgustin <aidinagustin2@gmail.com>
2023-12-04 23:14:05 +0700

    swiper

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -1,13 +1,16 @@
 @extends('layouts.main')
 
 @section('container')
-    <!-- Projects Section-->
+     <!-- Projects Section-->
     <section class="py-5">
-        <div class="container px-5 mb-5">
-            <div class="text-center mb-5">
-                <h1 class="display-5 fw-bolder mb-0"><span class="text-gradient d-inline">Projects</span></h1>
+        <div class="container px-5 mt-5">
+            <div class="text-center">
+                <h1 class="display-5 fw-bolder mb-2"><span class="text-gradient d-inline">Projects</span></h1>
+                <p class="text-muted">
+                    Berikut adalah kumpulan project yang telah dibuat.
+                </p>
             </div>
-            <div class="row gx-5 justify-content-center">
+            {{-- <div class="row gx-5 justify-content-center">
                 <div class="col-lg-11 col-xl-9 col-xxl-8">
                     <!-- Project Card 1-->
                     <div class="card overflow-hidden shadow rounded-4 border-0 mb-5">
@@ -34,16 +37,126 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> --}}
         </div>
     </section>
     <!-- Call to action section-->
-    <section class="py-5 bg-gradient-primary-to-secondary text-white">
+    {{-- <section class="py-5 bg-gradient-primary-to-secondary text-white">
         <div class="container px-5 my-5">
             <div class="text-center">
                 <h2 class="display-4 fw-bolder mb-4">Let's build something together</h2>
                 <a class="btn btn-outline-light btn-lg px-5 py-3 fs-6 fw-bolder" href="contact.html">Contact me</a>
             </div>
         </div>
-    </section>
+    </section>  --}}
+
+    <!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <title>Swiper demo</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
+  <!-- Link Swiper's CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+
+  <!-- Demo styles -->
+  <style>
+    html,
+    body {
+      position: relative;
+      height: 100%;
+    }
+
+    body {
+      background: #eee;
+      font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+      font-size: 14px;
+      color: #000;
+      margin: 0;
+      padding: 0;
+    }
+
+    .swiper {
+      width: 100%;
+      padding-top: 50px;
+      padding-bottom: 50px;
+      
+    }
+
+    .swiper-slide {
+      background-position: center;
+      background-size: cover;
+      width: 40%;
+      height: 70%;
+    }
+
+    .swiper-slide img {
+      display: block;
+      width: 100%;
+    }
+  </style>
+</head>
+
+<body>
+  <!-- Swiper -->
+  <div class="swiper mySwiper">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">
+        <img src="assets/castro.png" />
+      </div>
+      <div class="swiper-slide">
+        <img src="assets/castro.png" />
+      </div>
+      <div class="swiper-slide">
+        <img src="assets/castro.png" />
+      </div>
+      <div class="swiper-slide">
+        <img src="assets/castro.png" />
+      </div>
+      <div class="swiper-slide">
+        <img src="assets/castro.png" />
+      </div>
+      {{-- <div class="swiper-slide">
+        <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+      </div>
+      <div class="swiper-slide">
+        <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+      </div>
+      <div class="swiper-slide">
+        <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+      </div>
+      <div class="swiper-slide">
+        <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+      </div> --}}
+    </div>
+    <div class="swiper-pagination"></div>
+  </div>
+
+  <!-- Swiper JS -->
+  <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+  <!-- Initialize Swiper -->
+  <script>
+    var swiper = new Swiper(".mySwiper", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
+  </script>
+</body>
+
+</html>
+
 @endsection
