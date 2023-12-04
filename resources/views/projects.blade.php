243ae1d648e319125cc7f0ec295053927ebcd435
AiDinaAgustin <aidinagustin2@gmail.com>
2023-12-04 22:40:37 +0700

    upload dulu seadanya

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -14,10 +14,10 @@
                         <div class="card-body p-0">
                             <div class="d-flex align-items-center">
                                 <div class="p-5">
-                                    <h2 class="fw-bolder">Project Name 1</h2>
+                                    <h2 class="fw-bolder">Castro</h2>
                                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius at enim eum illum aperiam placeat esse? Mollitia omnis minima saepe recusandae libero, iste ad asperiores! Explicabo commodi quo itaque! Ipsam!</p>
                                 </div>
-                                <img class="img-fluid" src="https://dummyimage.com/300x400/343a40/6c757d" alt="..." />
+                                <img class="img-fluid" src="assets/castro.png" alt="..." width="300" height="auto" />
                             </div>
                         </div>
                     </div>
@@ -26,7 +26,7 @@
                         <div class="card-body p-0">
                             <div class="d-flex align-items-center">
                                 <div class="p-5">
-                                    <h2 class="fw-bolder">Project Name 2</h2>
+                                    <h2 class="fw-bolder">My Portofolio</h2>
                                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius at enim eum illum aperiam placeat esse? Mollitia omnis minima saepe recusandae libero, iste ad asperiores! Explicabo commodi quo itaque! Ipsam!</p>
                                 </div>
                                 <img class="img-fluid" src="https://dummyimage.com/300x400/343a40/6c757d" alt="..." />
