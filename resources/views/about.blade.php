41277717d1bb16deda5ff1877624048ad191a888
AiDinaAgustin <aidinagustin2@gmail.com>
2023-12-04 21:52:57 +0700

    about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,15 +6,21 @@
         <div class="row gx-5 justify-content-center">
             <div class="col-xxl-8">
                 <div class="text-center my-5">
-                    <h2 class="display-5 fw-bolder"><span class="text-gradient d-inline">About Me</span></h2>
-                    <p class="lead fw-light mb-4">My name is Ai Dina Agustin.</p>
-                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit dolorum itaque qui unde quisquam consequatur autem. Eveniet quasi nobis aliquid cumque officiis sed rem iure ipsa! Praesentium ratione atque dolorem?. 
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae commodi officia ex impedit delectus. Mollitia autem, tempora, sint a deserunt odit itaque numquam error sapiente tempore optio distinctio ex ratione.
-                    </p>
-                    <div class="d-flex justify-content-center fs-2 gap-4">
-                        <a class="text-gradient" href="#!"><i class="bi bi-twitter"></i></a>
-                        <a class="text-gradient" href="#!"><i class="bi bi-linkedin"></i></a>
-                        <a class="text-gradient" href="#!"><i class="bi bi-github"></i></a>
+                    
+                        <h2 class="display-5 fw-bolder"><span class="text-gradient d-inline">About Me</span></h2>
+                        <p class="lead fw-light mb-4">My name is Ai Dina Agustin.</p>
+                    <div data-aos="fade-up">
+                        <p class="text-muted">
+                            I am a 5th-semester student of the Informatics Engineering Department at Nusa Putra University. With an GPA of 3.86, I have a strong interest in technology, especially in web development. I have experience in web development on both the front-end and back-end. I am proficient in various programming languages, such as HTML, CSS, JavaScript, PHP, and MySQL. I also have experience using various frameworks and libraries, such as Laravel, CodeIgniter, Bootstrap, and Tailwind.
+                        </p>
+                    </div>
+                    <div data-aos="flip-left"></div>
+                        <div class="d-flex justify-content-center fs-2 gap-4">
+                            <a class="text-gradient" href="https://www.facebook.com/faujiyah.r.azizah"><i class="bi bi-facebook"></i></a>
+                            <a class="text-gradient" href="https://www.instagram.com/sana.agustina/?igshid=OGQ5ZDc2ODk2ZA%3D%3D"><i class="bi bi-instagram"></i></a>
+                            <a class="text-gradient" href="https://www.linkedin.com/in/aidina-agustin-b49806223/"><i class="bi bi-linkedin"></i></a>
+                            <a class="text-gradient" href="https://github.com/AiDinaAgustin"><i class="bi bi-github"></i></a>
+                        </div>
                     </div>
                 </div>
             </div>
