3968b25f453af7116ce95dbca4b0dc68d0fabc7a
AiDinaAgustin <aidinagustin2@gmail.com>
2023-12-04 22:25:27 +0700

    fix navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-white py-3">
+<nav class="navbar navbar-expand-lg navbar-light bg-white py-6 fixed-top">
     <div class="container px-5">
         <a class="navbar-brand" href="index.html"><span class="fw-bolder text-primary">Ai Dina Agustin</span></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
